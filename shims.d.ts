8627eadc1475c5b5505a744924464092f40737a8
J0EK3R <J0EK3R@users.noreply.github.com>
2024-12-20 14:21:45 +0100

    JK: Test

--- shims.d.ts
+++ shims.d.ts
@@ -2,9 +2,9 @@
 
 
     /**
-     * Support for MK6Hub.
+     * Support for MK6 Hub.
      */
-    //% color=#00c300 weight=100 icon="\uf294" block="MK6Hub"
+    //% color=#00c300 weight=100 icon="\uf294" block="MK6"
 declare namespace mk6hub {
 }
 
--- test.ts
+++ test.ts
@@ -1,10 +1,5 @@
-// mk6hub.start("0f0f0f0f0f"); // this is fake HWID
+// mk6hub.start(); /
 
 // mk6hub.stop();
-
-// mk6hub.startWithDeviceMessage(
-//     "0f0f0f0f0f",   // this is also fake HWID
-//     "1a2b3c4d5e6f70809010a0b0c0"
-// );
 
 // mk6hub.stop();
