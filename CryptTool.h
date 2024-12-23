ee4c7816385105c0f44bdef08ee9a7a48c2f9ff0
J0EK3R <J0EK3R@gmx.net>
2024-12-23 05:55:02 +0100

    JK: Test

---
+++ CryptTool.h
@@ -0,0 +1,10 @@
+#ifndef CRYPTTOOL_H
+#define CRYPTTOOL_H
+
+#include "MicroBitConfig.h"
+
+class CryptTool
+{
+public:
+    uint8_t *EnCrypt(uint8_t *addr, size_t addrLength, uint8_t *data, size_t dataLength, uint8_t ctxValue);
+};
