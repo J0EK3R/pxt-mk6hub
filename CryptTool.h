35bd019cd2460f59278207a84eed7c149749f74e
J0EK3R <J0EK3R@gmx.net>
2024-12-25 05:36:54 +0100

    JK: Test

--- CryptTool.h
+++ CryptTool.h
@@ -11,11 +11,11 @@
 class CryptTool
 {
     public:
-        void get_rf_payload(const uint8_t *addr, const uint8_t addrLength, const uint8_t *data, const uint8_t dataLength, const uint8_t ctxValue);
+    void get_rf_payload(const uint8_t *addr, const uint8_t addrLength, const uint8_t *data, const uint8_t dataLength, const uint8_t ctxValue);
 
-    private:
-        const uint8_t ctxValue = 0x25;
-        const uint8_t addressArray[5] = {0xC1, 0xC2, 0xC3, 0xC4, 0xC5};
+    // private:
+    // const uint8_t ctxValue = 0x25;
+    // const uint8_t addressArray[5] = {0xC1, 0xC2, 0xC3, 0xC4, 0xC5};
 };
 
 //================================================================
